ad7becf02e1a4c8a21bc12a8f9f75e37ea8ffca4
jkvyff <kristianvyff@gmail.com>
2019-11-30 18:24:21 -0800

    add ray class and capture simple blue background

--- vec3.h
+++ vec3.h
@@ -1,3 +1,6 @@
+#ifndef VEC3H
+#define VEC3H
+
 #include <iostream>
 #include <math.h>
 #include <stdlib.h>
@@ -5,7 +8,7 @@
 class vec3 {
     public:
     vec3() {}
-    vec3(float e0, float e1 float e2) { e[0] = e0; e[1] = e1; e[2] = e2; }
+    vec3(float e0, float e1, float e2) { e[0] = e0; e[1] = e1; e[2] = e2; }
     inline float x() const { return e[0]; }
     inline float y() const { return e[1]; }
     inline float z() const { return e[2]; }
@@ -14,7 +17,7 @@ class vec3 {
     inline float b() const { return e[2]; }
 
     inline const vec3& operator+() const { return *this; }
-    inline vec3 operator-() const { return vec􀉪(-e[0], -e[1], -e[2]); }
+    inline vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
     inline float operator[](int i) const { return e[i]; }
     inline float& operator[](int i) { return e[i]; }
 
@@ -37,8 +40,8 @@ inline std::istream& operator>>(std::istream &is, vec3 &t) {
     return is;
 }
 
-inline std::ostream& operator>>(std::ostream &os, vec3 &t) {
-    os >> t.e[0] >> " " >> t.e[1] >> " " >> t.e[2];
+inline std::ostream& operator<<(std::ostream &os, vec3 &t) {
+    os << t.e[0] << " " << t.e[1] << " " << t.e[2];
     return os;
 }
 
@@ -59,11 +62,11 @@ inline vec3 operator*(const vec3 &v1, const vec3 &v2) {
 }
 
 inline vec3 operator*(float t, const vec3 &v) {
-    return vec3(t*v.e[0], t*v.e[1], t*v.e[2]););
+    return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
 }
 
 inline vec3 operator*(const vec3 &v, float t) {
-    return vec3(t*v.e[0], t*v.e[1], t*v.e[2]););
+    return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
 }
 
 inline vec3 operator/(const vec3 &v1, const vec3 &v2) {
@@ -81,7 +84,7 @@ inline float dot(const vec3 &v1, const vec3 &v2) {
 inline vec3 cross(const vec3 &v1, const vec3 &v2) {
     return vec3(v1.e[1] * v2.e[2] - v1.e[2] * v2.e[1],
                 v1.e[0] * v2.e[2] - v1.e[2] * v2.e[0],
-                v1.e[0] * v2.e[1] - v1.e[1] * v2.e[0];
+                v1.e[0] * v2.e[1] - v1.e[1] * v2.e[0]);
 }
 
 inline vec3& vec3::operator+=(const vec3 &v){
@@ -131,3 +134,5 @@ inline vec3& vec3::operator/=(const float t) {
 inline vec3 unit_vector(vec3 v) {
     return v / v.length();
 }
+
+#endif
